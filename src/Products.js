cded0e86281c440cb2a918917b107e9d4880e439
SylvesterFraide <fridaysylvester2@gmail.com>
2024-07-19 14:12:57 +0300

    more products

--- src/Products.js
+++ src/Products.js
@@ -5,6 +5,15 @@ import product4 from './assets/laptop2.jpg';
 import product5 from './assets/iphone7.jpg';
 import product6 from './assets/mouse4.jpg';
 import product7 from './assets/keyboard3.jpg';
+import image2 from './assets/image2.jpg';
+import image3 from './assets/image3.png';
+import image4 from './assets/image4.webp';
+import image5 from './assets/image5.webp';
+import image6 from './assets/image6.webp';
+import image7 from './assets/image7.webp';
+import image8 from './assets/image8.webp';
+import image9 from './assets/image9.webp';
+import image10 from './assets/image10.webp';
 
 
 export const PRODUCTS = [
@@ -61,5 +70,68 @@ export const PRODUCTS = [
         price: 1200,
         productImage: product7,
 
+    },
+
+    {
+        id: 8,
+        productName: "Sumsung S24 Titanium",
+        price: 255000,
+        productImage: image2,
+    },
+
+    {
+        id: 9,
+        productName: "Iphone 15 Promax",
+        price: 265000,
+        productImage: image3,
+    },
+
+    {
+        id: 10,
+        productName: "Sumsung Galaxy A24",
+        price: 26500,
+        productImage: image4,
+    },
+
+    {
+        id: 11,
+        productName: "Google Pixel",
+        price: 37000,
+        productImage: image5,
+    },
+
+    {
+        id: 12,
+        productName: "Sumsung Galaxy A15",
+        price: 16000,
+        productImage: image6,
+    },
+
+    {
+        id: 13,
+        productName: "Redmi Note 13",
+        price: 21000,
+        productImage: image7,
+    },
+
+    {
+        id: 14,
+        productName: "Oppo A18",
+        price: 17000,
+        productImage: image8,
+    },
+
+    {
+        id: 15,
+        productName: "Nokia g21",
+        price: 20000,
+        productImage: image9,
+    },
+
+    {
+        id: 16,
+        productName: "Xiaomi Redmi 13c",
+        price: 13000,
+        productImage: image10,
     },
  ];
